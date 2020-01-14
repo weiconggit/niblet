7c00bffa50f2d35949a485c2a97dc1afd7628389
weicong <weicongmail@foxmail.com>
2020-01-14 17:25:09 +0800

    del dead code

--- src/main/java/org/weicong/sys/DetectionEndpoint.java
+++ src/main/java/org/weicong/sys/DetectionEndpoint.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.weicong.auth.TokenUtil;
 import org.weicong.auth.config.CustomUser;
